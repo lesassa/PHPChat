b5454ca462ee567ef03ce69325a8a119a82f1e09
tie303761 <tie303761@S1110C00045-T1.tisnt.tis.co.jp>
2017-06-23 19:57:40 +0900

    休日対応

--- public/src/Controller/Component/AIComponent.php
+++ public/src/Controller/Component/AIComponent.php
@@ -26,45 +26,26 @@ class AIComponent  extends Component
 	 */
 	function talkAI($text) {
 
-		if ($text == "今日の勤怠") {
-			$obj = PHPExcel_IOFactory::createReader('Excel2007');
-			$book = $obj->load(self::FILE);
 
+		if (preg_match("/勤怠/", $text)) {
 
-			$month = date ("n");
-			$day = date ("j");
-
-
-			//シートを設定する
-			$sheetName = $month."月";
-			$book->setActiveSheetIndexByName($sheetName);//一番最初のシートを選択
-			$sheet = $book->getActiveSheet();//選択シートにアクセスを開始
-			$todayRow = self::ROW_OFFSET + $day;
-			$today = $sheet->getCellByColumnAndRow(1, $todayRow)->getFormattedValue();
-			$reply = $today."の勤怠は";
-
-
-			$i = 0;
-			$memberColumn = self::NAME_COLUMN  + (self::NAME_INDENT * $i);
-			$memberName = $sheet->getCellByColumnAndRow($memberColumn, self::NAME_ROW)->getFormattedValue();
-			while ($memberName != "") {
-				$reply .= PHP_EOL;
-				$startTime = $sheet->getCellByColumnAndRow($memberColumn, $todayRow)->getFormattedValue();
-
-				if ($startTime == "") {
-					$service = $sheet->getCellByColumnAndRow($memberColumn + 2, $todayRow)->getFormattedValue();
-				} else {
-
-					$endTime = $sheet->getCellByColumnAndRow($memberColumn + 1, $todayRow)->getFormattedValue();
-					$service = $startTime."-".$endTime;
-				}
-				$reply .= $memberName."さんは".$service."だって";
-
-				$i++;
-				$memberColumn = self::NAME_COLUMN  + (self::NAME_INDENT * $i);
-				$memberName = $sheet->getCellByColumnAndRow($memberColumn, self::NAME_ROW)->getFormattedValue();
+			if (preg_match("/今日/", $text)) { //今日
+				$month = date ("n");
+				$day = date ("j");
+			} else if (preg_match("/明日/", $text)) { //明日
+				$month = date('n', strtotime('+1 day'));
+				$day = date('j', strtotime('+1 day'));
+			} else if (preg_match("/[0-9]{1,2}\/[0-9]{1,2}/", $text, $date)) { //日付指定
+				$date = explode ("/", $date[0]);
+				$month = $date[0];
+				$day = $date[1];
+				$month= ltrim($month, '0');
+				$day= ltrim($day, '0');
+			} else {
+				return "何をおっしゃっているの？";
 			}
 
+			$reply = $this->getServiceMessage($month, $day);
 
 			//ログ出力
 			$this->Log->outputLog($reply);
@@ -78,5 +59,60 @@ class AIComponent  extends Component
 		return $reply;
 	}
 
+
+	private function getServiceMessage($month, $day) {
+
+		$obj = PHPExcel_IOFactory::createReader('Excel2007');
+		$book = $obj->load(self::FILE);
+
+		//シートを設定する
+		$sheetName = $month."月";
+		$book->setActiveSheetIndexByName($sheetName);//一番最初のシートを選択
+		$sheet = $book->getActiveSheet();//選択シートにアクセスを開始
+		$todayRow = self::ROW_OFFSET + $day;
+		$today = $sheet->getCellByColumnAndRow(1, $todayRow)->getFormattedValue();
+		$today= ltrim($today, '0');
+		$reply = $today."の勤怠は...";
+
+		//休日の場合
+		$holiday = $sheet->getCellByColumnAndRow(2, $todayRow)->getFormattedValue();
+		if ($holiday != "") {
+			$reply .= $today."は休日よ".PHP_EOL;
+			$reply .= "会社に来てはいけないのよ".PHP_EOL;
+			$reply .= "それでも私は働くわ";
+		}
+
+		$i = 0;
+		$memberColumn = self::NAME_COLUMN  + (self::NAME_INDENT * $i);
+		$memberName = $sheet->getCellByColumnAndRow($memberColumn, self::NAME_ROW)->getFormattedValue();
+		while ($memberName != "") {
+
+			$startTime = $sheet->getCellByColumnAndRow($memberColumn, $todayRow)->getFormattedValue();
+
+			if ($startTime == "") {
+				$service = $sheet->getCellByColumnAndRow($memberColumn + 2, $todayRow)->getFormattedValue();
+			} else {
+
+				$endTime = $sheet->getCellByColumnAndRow($memberColumn + 1, $todayRow)->getFormattedValue();
+				$service = $startTime."-".$endTime;
+			}
+
+			//個人メッセージ作成
+			if ($holiday != "") {
+				if ($startTime != "") {
+					$reply .= PHP_EOL;
+					$reply .= $memberName."さんも".$service."に働くのね";
+				}
+			} else {
+				$reply .= PHP_EOL;
+				$reply .= $memberName."さんは".$service."だって";
+			}
+
+			$i++;
+			$memberColumn = self::NAME_COLUMN  + (self::NAME_INDENT * $i);
+			$memberName = $sheet->getCellByColumnAndRow($memberColumn, self::NAME_ROW)->getFormattedValue();
+		}
+		return $reply;
+	}
 }
 ?>
